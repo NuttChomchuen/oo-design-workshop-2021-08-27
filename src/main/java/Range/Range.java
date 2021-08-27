8ebb9910b0338964b967ddee6348f6730aa0f7a2
https://github.com/NuttChomchuen/oo-design-workshop.git <nutt.chomchuen@gmail.com>
2021-08-27 16:11:32 +0700

    Range

---
+++ src/main/java/Range/Range.java
@@ -0,0 +1,13 @@
+package Range;
+
+public class Range {
+    private String input;
+
+    public Range(String input){
+
+    }
+
+    public boolean isStartWithInclusive() {
+        return true;
+    }
+}
---
+++ src/test/java/RangeTest.java
@@ -0,0 +1,18 @@
+import Range.Range;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RangeTest {
+    @Test
+    public void isStartWitchInclusiveWithKampooShouldBetrue()
+    {
+        // Arrage
+        Range range = new Range("[1,5]");
+
+        // Act
+        boolean actualResult = range.isStartWithInclusive();
+
+        // Assert
+        assertTrue(actualResult);
+    }
+}
